8c10d7c931b73cc67a127fe5cf2be26b53171979
Jacob Lifshay <programmerjake@gmail.com>
2015-07-08 09:01:07 -0600

    work on build.cmd; still doesn't work

--- build.cmd
+++ build.cmd
@@ -11,5 +11,7 @@ pause
 exit /b
 
 :construct_file_list
-set file_list=%file_list% %1
+set object_file="%~1.obj"
+cl /c /Fo%object_file% /Z7 /EHsc /I external-lib /I include /I external-lib/zlib-1.2.8 /I external-lib/SDL2-2.0.3/include /I external-lib/lpng1617 /I external-lib/libvorbis-1.3.5/include /I external-lib/libogg-1.3.2/include %1
+set file_list=%file_list% %object_file%
 exit /b
