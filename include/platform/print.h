ad7165a18eea4ae24b74e7e10a8fe8ab7ad2cc0f
Jacob Lifshay <programmerjake@gmail.com>
2015-09-12 22:26:08 -0600

    added printing

---
+++ include/platform/print.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2012-2015 Jacob R. Lifshay
+ * This file is part of Voxels.
+ *
+ * Voxels is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Voxels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Voxels; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef PRINT_H_INCLUDED
+#define PRINT_H_INCLUDED
+
+#include <stdexcept>
+#include <string>
+#include <memory>
+#include <vector>
+#include "util/enum_traits.h"
+
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+class PrintingException : public std::runtime_error
+{
+public:
+    explicit PrintingException(std::string msg)
+        : runtime_error(msg)
+    {
+    }
+};
+
+class PrintFailedException : public PrintingException
+{
+public:
+    explicit PrintFailedException(std::string msg)
+        : PrintingException(msg)
+    {
+    }
+};
+
+class CancelFailedException : public PrintingException
+{
+public:
+    explicit CancelFailedException(std::string msg)
+        : PrintingException(msg)
+    {
+    }
+};
+
+class Printer;
+
+GCC_PRAGMA(diagnostic push)
+GCC_PRAGMA(diagnostic ignored "-Weffc++")
+GCC_PRAGMA(diagnostic ignored "-Wnon-virtual-dtor")
+class Printer : public std::enable_shared_from_this<Printer>
+{
+GCC_PRAGMA(diagnostic pop)
+    Printer(const Printer &) = delete;
+    Printer &operator =(const Printer &) = delete;
+public:
+    const std::wstring id;
+    const std::wstring name; // human-readable name. eg: "My Printer"
+    const std::wstring type; // printer type. eg: "HP LaserJet 4000 Series"
+    const std::wstring location; // printer location. eg: "Room 201"
+    const bool isOnline;
+    explicit Printer(std::wstring id, std::wstring name, std::wstring type, std::wstring location, bool isOnline)
+        : id(id), name(name), type(type), location(location), isOnline(isOnline)
+    {
+    }
+    virtual ~Printer() = default;
+    /** print text
+     * @param text the text to print
+     * @param the name to use for the job
+     * @throw PrintFailedException if there was an error printing
+     */
+    virtual void printText(std::wstring text, std::wstring jobName) const = 0;
+};
+
+/** get the list of printers
+ * @return the list of printers with the default printer listed first
+ * @throw PrintingException when the printer system fails
+ */
+std::vector<std::shared_ptr<Printer>> getPrinterList();
+
+}
+}
+}
+
+#endif // PRINT_H_INCLUDED
---
+++ src/platform/print.cpp
@@ -0,0 +1,246 @@
+/*
+ * Copyright (C) 2012-2015 Jacob R. Lifshay
+ * This file is part of Voxels.
+ *
+ * Voxels is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Voxels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Voxels; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#include "platform/print.h"
+#include "util/util.h"
+
+#if _WIN64 || _WIN32
+FIXME_MESSAGE("implement printing for windows")
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+std::vector<std::shared_ptr<Printer>> getPrinterList()
+{
+    throw PrintingException("printing not implemented for windows");
+}
+}
+}
+}
+#elif __ANDROID
+FIXME_MESSAGE("implement printing for android")
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+std::vector<std::shared_ptr<Printer>> getPrinterList()
+{
+    throw PrintingException("printing not implemented for android");
+}
+}
+}
+}
+#elif __APPLE__
+FIXME_MESSAGE("implement printing for apple")
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+std::vector<std::shared_ptr<Printer>> getPrinterList()
+{
+    throw PrintingException("printing not implemented for apple");
+}
+}
+}
+}
+#elif __linux || __unix
+#include <cups/cups.h>
+#include <cstring>
+#include "util/string_cast.h"
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+namespace
+{
+std::vector<std::shared_ptr<cups_option_t>> getOptions(std::shared_ptr<cups_dest_t> dest)
+{
+    assert(dest != nullptr);
+    std::vector<std::shared_ptr<cups_option_t>> retval;
+    retval.reserve((std::size_t)dest->num_options);
+    for(std::size_t i = 0; i < (std::size_t)dest->num_options; i++)
+    {
+        retval.emplace_back(dest, &dest->options[i]);
+    }
+    return std::move(retval);
+}
+
+class CUPSPrinter final : public Printer
+{
+private:
+    explicit CUPSPrinter(std::wstring id,
+                         std::wstring name,
+                         std::wstring type,
+                         std::wstring location,
+                         bool isOnline,
+                         std::shared_ptr<cups_dest_t> dest)
+        : Printer(id, name, type, location, isOnline),
+        dest(dest)
+    {
+    }
+public:
+    const std::shared_ptr<cups_dest_t> dest;
+    static std::shared_ptr<CUPSPrinter> make(std::shared_ptr<cups_dest_t> dest)
+    {
+        assert(dest != nullptr);
+        std::wstring id = string_cast<std::wstring>(dest->name);
+        if(dest->instance != nullptr)
+            id += L"/" + string_cast<std::wstring>(dest->instance);
+        std::wstring name = id, type = L"", location = L"";
+        bool isOnline = true;
+        for(std::size_t optionIndex = 0; optionIndex < (std::size_t)dest->num_options; optionIndex++)
+        {
+            const cups_option_t &option = dest->options[optionIndex];
+            if(std::strcmp(option.name, "printer-info") == 0)
+            {
+                name = string_cast<std::wstring>(option.value);
+            }
+            else if(std::strcmp(option.name, "printer-location") == 0)
+            {
+                location = string_cast<std::wstring>(option.value);
+            }
+            else if(std::strcmp(option.name, "printer-make-and-model") == 0)
+            {
+                type = string_cast<std::wstring>(option.value);
+            }
+            else if(std::strcmp(option.name, "printer-is-accepting-jobs") == 0)
+            {
+                if(std::strcmp(option.value, "true") != 0)
+                    isOnline = false;
+            }
+        }
+        return std::shared_ptr<CUPSPrinter>(new CUPSPrinter(id, name, type, location, isOnline, dest));
+    }
+
+    /** print text
+     * @param text the text to print
+     * @param the name to use for the job
+     * @throw PrintFailedException if there was an error printing
+     */
+    virtual void printText(std::wstring text, std::wstring jobName) const override
+    {
+        cups_option_t *options = nullptr;
+        int optionCount = 0;
+        optionCount = cupsAddOption("attributes-charset", "utf-8", optionCount, &options);
+        int jobId = cupsCreateJob(CUPS_HTTP_DEFAULT, dest->name, string_cast<std::string>(jobName).c_str(), optionCount, options);
+        cupsFreeOptions(optionCount, options);
+        if(jobId == 0)
+            throw PrintFailedException(std::string("cupsCreateJob failed: ") + cupsLastErrorString());
+        cupsStartDocument(CUPS_HTTP_DEFAULT, dest->name, jobId, string_cast<std::string>(jobName).c_str(), CUPS_FORMAT_TEXT, 1);
+        std::string utf8Text = string_cast<std::string>(text);
+        cupsWriteRequestData(CUPS_HTTP_DEFAULT, utf8Text.data(), utf8Text.size());
+        cupsFinishDocument(CUPS_HTTP_DEFAULT, dest->name);
+    }
+};
+}
+
+std::vector<std::shared_ptr<Printer>> getPrinterList()
+{
+    std::vector<std::shared_ptr<Printer>> retval;
+    cups_dest_t *dests = nullptr;
+    int destCount = cupsGetDests(&dests);
+    std::shared_ptr<cups_dest_t> destsAsSharedPtr = std::shared_ptr<cups_dest_t>(dests, [destCount](cups_dest_t *dests)
+    {
+        cupsFreeDests(destCount, dests);
+    });
+    retval.reserve((std::size_t)destCount);
+    retval.push_back(nullptr); // slot for default printer
+    std::shared_ptr<Printer> defaultPrinter = nullptr;
+    for(std::size_t destIndex = 0; destIndex < (std::size_t)destCount; destIndex++)
+    {
+        std::shared_ptr<cups_dest_t> dest = std::shared_ptr<cups_dest_t>(destsAsSharedPtr, &dests[destIndex]);
+        std::shared_ptr<Printer> printer = CUPSPrinter::make(dest);
+        if(dest->is_default && defaultPrinter == nullptr) // in case cups returns multiple defaults
+        {
+            defaultPrinter = std::move(printer);
+            continue;
+        }
+        retval.push_back(printer);
+    }
+    if(defaultPrinter == nullptr)
+        retval.erase(retval.begin()); // erase empty slot
+    else
+        retval.front() = std::move(defaultPrinter);
+    return std::move(retval);
+}
+}
+}
+}
+#elif __posix
+FIXME_MESSAGE("implement printing for other posix")
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+std::vector<std::shared_ptr<Printer>> getPrinterList()
+{
+    throw PrintingException("printing not implemented for other posix");
+}
+}
+}
+}
+#else
+#error unknown platform in print.cpp
+#endif
+
+#if 0
+#include <iostream>
+#include <cstdlib>
+#include "util/util.h"
+namespace programmerjake
+{
+namespace voxels
+{
+namespace Printing
+{
+namespace
+{
+initializer init1([]()
+{
+    std::vector<std::shared_ptr<Printer>> printers = getPrinterList();
+    std::shared_ptr<Printer> defaultPrinter = nullptr;
+    for(std::shared_ptr<Printer> printer : printers)
+    {
+        if(defaultPrinter == nullptr)
+            defaultPrinter = printer;
+        std::wcout << L"Printer:\nid='" << printer->id << L"'\nname='" << printer->name;
+        std::wcout << L"'\ntype='" << printer->type << L"'\nlocation='" << printer->location;
+        std::wcout << L"'\nisOnline=" << (printer->isOnline ? L"true" : L"false") << L"\n" << std::endl;
+    }
+    printers.clear();
+    defaultPrinter->printText(L"This is a printer code test.\nLine 2.\fPage 2.", L"Printer Code Test");
+    defaultPrinter = nullptr;
+    std::exit(0);
+});
+}
+}
+}
+}
+#endif
